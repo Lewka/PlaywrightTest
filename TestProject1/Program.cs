7aae842df76820aaa2074cc118fc39e0a8bb95cb
Alexey Andreikin <aandreikin@aras.com>
2020-10-25 15:19:49 +0300

    test

---
+++ TestProject1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestProject1
+{
+	public class Program
+	{
+		public static void Main(string[] a)
+		{
+			
+		}
+	}
+}
---
+++ TestProject1/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PlaywrightSharp;
+
+namespace TestProject1
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public async Task Test1()
+		{
+			Console.WriteLine("Installing playwright");
+			await Playwright.InstallAsync();
+			Console.WriteLine("Creating playwright");
+			var playwright = await Playwright.CreateAsync();
+		}
+	}
+}
